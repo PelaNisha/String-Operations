1b2f873296314bec9893e7370a93de7d029fa79b
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-05-24 22:16:47 +0545

    New commit

--- dfa_test.py
+++ dfa_test.py
@@ -1,15 +1,43 @@
 # Construct a DFA over (a,b) that do not end with 'ba'
 
+accept_states = ['q0','q1']
+transition = {
+	'q0':{'a':'q0','b':'q1' },
+	'q1':{'a':'q2','b':'q1' },
+	'q2':{'a':'q0','b':'q1' }
+	}
+
+
 def split(word):
 	li = []
 	for char in word:
 		li.append(char)
 	return li	
 
+
 word = input("Enter the string: ")
 list_word = split(word)
 index = len(word)
-if list_word[index-2]== 'b' and list_word[index-1]=='a':
-	print("String not accepted!")
+
+
+# Function for analize the string
+def func():
+	if index>1:
+		current_state = 'q0'
+		for i in range(0,index):
+			new_state = transition[current_state][list_word[i]]
+			print(current_state+" -> "+new_state)
+			current_state = new_state
+		return current_state	
+			
+	else:
+		return "string size is less"
+
+
+# Function calling
+x = func()
+
+if x in accept_states:
+	print("String is valid!")
 else:
-	print("Accepted!")	
+	print("String is invalid!")	
