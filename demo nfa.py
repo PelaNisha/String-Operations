18698d9d7213f7d2c6f3faddc046d4124ba55950
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-06-13 20:58:30 +0545

    added improvised code

--- demo nfa.py
+++ demo nfa.py
@@ -1,67 +1,49 @@
-# Construct a NFA over (a,b) that do not end with 'aa'
+# Construct a NFA over (a,b) that has "aa" as a substring
 
-accept_states = ['q2']
+
+accept_states = 'q2'
 transition = {
-	'q_0':{'a':['q_0','q_1'],'b':'q_0' },
-	'q_1':{'a':'q_2','b':'q_1' },
-	'q_2':{'a':'q_2','b':'q_2' }
+	'q0':{'a':['q0','q1'],'b':['q0'] },
+	'q1':{'a':['q2'],'b':["don't care"]},
+	'q2':{'a':['q2'],'b':['q2'] }
 	}
+
 
 # for input 
 word = input("Enter the string: ")
 list_word = list(word)
 word_length = len(word)
-
-def get_new_state(current_state,ch, index):
-	li = []
-	for states in current_state:
-		new_state = transition[states][ch]
-	print(new_state)
-	li.append(new_state)
-	func(new_state,index+1)
+current_state = ['q0']
 
 
-# Function for analize the string
-def func(current_state, index):
-	ch = list_word[index]
-	print("for '", ch,"'")
-	new_state = transition[current_state][ch]
-	print("New state ", new_state)
-	if len(new_state)!=3:
-		for item in new_state:
-			get_new_state(item, ch, index + 1)
+def func(current_state):
+	if word_length>1:	
+		for i in range(0,word_length):
+			li = []
+			ch = list_word[i]
+			print("for ",list_word[i])
+			for c in current_state:
+				if c == "don't care":
+					pass
+				else:
+					new_state = transition[c][ch]
+					print(str(c)+" -> "+str(new_state))
+					li.extend(new_state)
+					print('states:', li)
+			current_state = li
+			print("-----------")
+		return li	
+			
 	else:
-		current_state = new_state
-		print("new current state ", current_state)
-	func(current_state, index+1)	
+		print("String size is less")
+		quit()
 
 
-
-# check the intial length
-if word_length>2:
-# Function calling
-	current_state = 'q_0'
-	x = func(current_state, 0)
-else:
-	exit
+x = func(current_state)
+print("Final states ", x)
 
 
-# accepting cases
-if x in accept_states:
+if accept_states in x:
 	print("String is valid!")
 else:
 	print("String is invalid!")	
-
-# eg baab- valid:
-# for b
-# q0 -> q0
-# # for a
-# q0 -> q0, q1
-# # for a
-# q0 -> q0,q1
-# q1 -> q2
-# # for b
-# q0 -> q0
-# q1 -> don't care
-# q2 -> q2
-
