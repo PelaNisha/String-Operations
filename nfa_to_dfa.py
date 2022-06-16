a01bfd6845bdcfc098b8a022601077fd3d14f86e
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2022-06-16 23:02:16 +0545

    third commit

--- nfa_to_dfa.py
+++ nfa_to_dfa.py
@@ -3,7 +3,7 @@
 
 accept_state = 'q2'
 transition = {
-	'q0':{'0':['q0','q1'],'1':['q1'] },
+	'q0':{'0':['q0'],'1':['q0','q1'] },
 	'q1':{'0':['q2'],'1':['q2']},
 	'q2':{'0':['q2'],'1':['q2'] }
 	}
@@ -18,25 +18,31 @@ initial_state=['q0']
 
 
 def get_nfa_states(initial_state):
+    ini0 = []
+    ini1 = []
     start_state = initial_state
     final_list.append(start_state)
     for item in start_state:
         # print(item)
         state0 = transition[item]['0']
-        ini.append(state0)
+        ini0.append(state0)
     for item in start_state:
         state1 = transition[item]['1']
-        ini.append(state1)
+        ini1.append(state1)
+
 
         
-    print(start_state,"(0)","->" ,state0, ", ",start_state,"(1)","->" ,state1)
-    
-    for i in ini:
+    print(start_state,"(0)","->" ,ini0, ", ",start_state,"(1)","->" ,ini1)
+  
+    ini0.extend(ini1)
+    print("INI is ", ini0)
+    for i in ini0:
         if i not in final_list:
+            final_list.append(i)
+            print("NExt state ", i)
             get_nfa_states(i)
+            
     
-
-
 get_nfa_states(initial_state)
 
 # q0 (0) ->q0, q0 (1) -> q0q1
